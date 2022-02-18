2ffdeba93ccfc5773c4f522093502b8102d15eb7
Yi-Huaaa <41279685+Yi-Huaaa@users.noreply.github.com>
2022-02-18 21:36:22 +0800

    Add files via upload

--- SQA_fully/experiment/run_gset.sh
+++ SQA_fully/experiment/run_gset.sh
@@ -1,139 +1,72 @@
 #!/bin/bash
 
-#!/bin/bash
-for m in {1..50..1}
-do		
-	for n in {0..5..1}
-	do
-        nMul=$((2**$n))
-        NN=$(( 1024 * $nMul ))
-        echo M = ${m} N = ${NN}
-        nvcc -D N=$NN -D M=$m v5-1-9.cu -arch=sm_86 -lcublas -o M${m}N${NN}
-	done
-done
-<<'COMMENT'
-for m in {1..10..1}
-do		
-	# for i in $(seq $1 $2)
-	for i in {6..6..1}
-	do
-        echo M = ${m} N = 1024 Gset = G${i}
-        ./M${m}N1024 ~/dataSet/Gset/G${i} > ./Results/G${i}M${m}.out
-	done
-done
-COMMENT
-
 # <<'COMMENT'
-#G6-G13
-for m in {1..50..1}
+# exefile
+for m in {1..64..1} # 1-64
 do		
-	# for i in $(seq $1 $2)
-	for i in {6..13..1}
+	for n in {0..1..1} # 1024-2048, 0-1
 	do
-        echo M = ${MM} N = 1024 Gset = G${i}
-        ./M${m}N1024 ~/dataSet/Gset/G${i} > ./Results/G${i}M${m}.out
+        for g in {0..5..1} # G0, original = 8, from 2 to 32, 0 - 5
+        do
+        	for inc in {0..5..1} # original = 16, from 2 to 32, 0 - 5
+        	do 
+		        nMul=$((2**$n))
+		        NN=$(( 1024 * $nMul ))
+		        g0=$((2**$g))
+		        iV=$((2**$inc))
+		        echo M = ${m} N = ${NN} G0 = ${g0} incVal = ${iV}
+		        nvcc -D N=$NN -D M=$m -D G0=$g0 -D incVal=$iV v5-1-9.cu -arch=sm_86 -lcublas -o ./exeF/M${m}N${NN}G0${g0}IV${iV}
+        	done        
+        done
 	done
 done
 
-#G18-G21
-for m in {1..50..1}
-do		
-	# for i in $(seq $1 $2)
-	for i in {18..21..1}
-	do
-        echo M = ${MM} N = 1024 Gset = G${i}
-        ./M${m}N1024 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
+
+
+# 1024
+declare -a array1=(13 18 19 20 21 47 51 53 54)
+
+# for((i=0; i<${#array1[@]}; i++))
+# do
+# 	echo ${array1[i]}
+# done
+for m in {1..64..1}  # 1-64
+do	
+    for g in {0..5..1} # G0, original = 8, from 2 to 32
+    do
+    	for inc in {0..5..1} # original = 16, from 2 to 32
+    	do 
+	        for((gsetNum=0; gsetNum<${#array1[@]}; gsetNum++))
+	        do
+		        g0=$((2**$g))
+		        iV=$((2**$inc))
+	   			echo M${m}N1024G0${g0}IV${iV}  G${array1[gsetNum]}M${m}G0${g0}IV${iV}.out
+			    ./exeF/M${m}N1024G0${g0}IV${iV} ~/Gset/G${array1[gsetNum]} > ./Results/G${array1[gsetNum]}M${m}G0${g0}IV${iV}.out
+	        done
+    	done        
+    done
 done
-#G27-G34
-for m in {1..50..1}
-do		
-	for i in {27..34..1}
-	do
-        echo M = ${MM} N = 2048 Gset = G${i}
-        ./M${m}N2048 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G39-G42
-for m in {1..50..1}
-do		
-	for i in {39..43..1}
-	do
-        echo M = ${MM} N = 2048 Gset = G${i}
-        ./M${m}N2048 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G48-G50
-for m in {1..50..1}
-do		
-	for i in {48..50..1}
-	do
-        echo M = ${MM} N = 4096 Gset = G${i}
-        ./M${m}N4096 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G56-G57
-for m in {1..50..1}
-do		
-	for i in {56..57..1}
-	do
-        echo M = ${MM} N = 8192 Gset = G${i}
-        ./M${m}N8192 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G59
-for m in {1..50..1}
-do		
-	for i in {59..59..1}
-	do
-        echo M = ${MM} N = 8192 Gset = G${i}
-        ./M${m}N8192 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G61-G65
-for m in {1..50..1}
-do		
-	for i in {61..65..1}
-	do
-        echo M = ${MM} N = 8192 Gset = G${i}
-        ./M${m}N8192 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G66-G67
-for m in {1..50..1}
-do		
-	for i in {66..67..1}
-	do
-        echo M = ${MM} N = 16384 Gset = G${i}
-        ./M${m}N16384 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G72
-for m in {1..50..1}
-do		
-	for i in {72..72..1}
-	do
-        echo M = ${MM} N = 16384 Gset = G${i}
-        ./M${m}N16384 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G77
-for m in {1..50..1}
-do		
-	for i in {77..77..1}
-	do
-        echo M = ${MM} N = 16384 Gset = G${i}
-        ./M${m}N16384 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
-#G81
-for m in {1..50..1}
-do		
-	for i in {81..81..1}
-	do
-        echo M = ${MM} N = 32768 Gset = G${i}
-        ./M${m}N32768 ~/dataSet/Gset/G${i}  > ./Results/G${i}M${m}.out
-	done
-done
+
 # COMMENT
+
+#2048
+declare -a array2=(34 39 40 41 42)
+for m in {1..64..1}  # 1-64
+do	
+    for g in {0..5..1} # G0, original = 8, from 2 to 32
+    do
+    	for inc in {0..5..1} # original = 16, from 2 to 32
+    	do 
+	        for((gsetNum=0; gsetNum<${#array2[@]}; gsetNum++))
+	        do
+		        g0=$((2**$g))
+		        iV=$((2**$inc))
+	   			echo M${m}N2048G0${g0}IV${iV}  G${array2[gsetNum]}M${m}G0${g0}IV${iV}.out
+			    ./exeF/M${m}N2048G0${g0}IV${iV} ~/Gset/G${array2[gsetNum]} > ./Results/G${array2[gsetNum]}M${m}G0${g0}IV${iV}.out
+	        done
+    	done        
+    done
+done
+
+
 
